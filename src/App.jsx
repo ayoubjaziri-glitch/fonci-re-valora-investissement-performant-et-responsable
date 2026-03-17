ec6d3e9e3ada9c09b19767554b49b08871aa4613
Ayoub jaziri <ayoubcontact33@gmail.com>
2026-03-17 13:39:44 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/admin" element={<AdminBackOffice />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
